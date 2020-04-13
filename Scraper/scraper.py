ff8d6cbe48e509bf9cd0dea4dd13a48579040a88
Vince3000 <61778304+Vince3000@users.noreply.github.com>
2020-04-13 16:52:22 +0200

    MAJ de la doc

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
+"""
+Auteur : Vince
+Doc :
+Scraper permettant de récupérer les infos sur les pages d'une recherche Google.
+Afin de ne garder que les résultats pertinents, on limite le nombre de page retournées (en l'occurrence 5).
+Extrait : Le titre
+          URL
+          ShortDescription
+Utilise BeautifulSoup.
+"""
 from bs4 import BeautifulSoup
 from splinter import browser
 from pymongo import MongoClient
@@ -7,16 +17,12 @@ from pprint import pprint
 import urllib
 import requests
 import json
-import lxml.html
 
-
+#Connection à la BDD /!\ /!\ /!\ En local à changer
 client = MongoClient('192.168.99.100:27017')
 db=client.scrapervince
-#initialise le mot de à rechercher
 resultsGlob=[]
 
-
-#On recherche les URL du résultat de la recherche vec beautifulSoup
 """Fonctions qui récupère les URL des résultats de la recherche
     query = str mot  recherché
     nbpages = int nbre de pages à scraper
@@ -36,24 +42,20 @@ def getSites(query, nbpages):
 
         headers = {"user-agent": USER_AGENT}
         resp = requests.get(URL, headers=headers)
-        with open("fileTestListSites.json", "w") as mj:
-            if resp.status_code == 200:
-                soup = BeautifulSoup(resp.content, "html.parser")
-                for g in soup.find_all('div', class_='r'):
-                    anchors = g.find_all('a')
-                    if anchors:
-                        link = anchors[0]['href']
-                        title = g.find('h3').text
-                        shortdescription = g.find('a').text
-                        item = {
-                            "title": title,
-                            "link": link,
-                            "shortdescription": shortdescription
-                        }
-                        search.append(link)
-                        resultsGlob.append(item)#Résultat à intégrer dans la BDD pour éventuel recherche/mot clé et présentaion en nuage
-                        résultat = db.reviews.insert_one (item)      
+        if resp.status_code == 200:
+            soup = BeautifulSoup(resp.content, "html.parser")
+            for g in soup.find_all('div', class_='r'):
+                anchors = g.find_all('a')
+                if anchors:
+                    link = anchors[0]['href']
+                    title = g.find('h3').text
+                    shortdescription = g.find('a').text
+                    item = {
+                        "title": title,
+                        "link": link,
+                        "shortdescription": shortdescription
+                    }
+                    résultat = db.reviews.insert_one (item)      
         pagenum=pagenum+1
-    return(search)
 
-search = getSites("covid 19",5)
+getSites("covid 19",5)
