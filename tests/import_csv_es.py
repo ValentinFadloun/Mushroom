8edad4804d8eb1ff64344216c5994225c7cda800
Valentin Fadloun <v.fadloun@gmail.com>
2020-04-24 09:22:24 +0200

    Import de données kibana pour test

---
+++ tests/import_csv_es.py
@@ -0,0 +1,20 @@
+# Import d'elasticsearch
+from elasticsearch import Elasticsearch, helpers
+
+es = Elasticsearch([{'host': '192.168.99.100', 'port': '9200'}], http_auth=('elastic', 'changeme'))
+
+import json
+
+def generate_data():
+    # Ouverture du fichier csv
+    with open('C:\\Users\\vfadl\\Documents\\Formation_CAPGEMINI\\projet\\Mushroom\\DATA\\shortversion.json','r') as jsonfile:
+        data = json.load(jsonfile)
+        for json_object in data["Countries"]:
+            yield {
+                "_index": "sars_2003",
+                "_type":"doc",
+                "_source": json_object
+            }
+
+
+helpers.bulk(es, generate_data())
